92ee206d045beb449d1c0f13b4c48b90633d09a4
Jason Staten <jstaten07@gmail.com>
2011-09-06 22:17:32 -0600

    Added todoItem

--- script/app.js
+++ script/app.js
@@ -2,13 +2,39 @@ function App() {
 
   var viewModel = {
     items: ko.observableArray(),
+    currentText: ko.observable(''),
+
     addItem: function(item){
       viewModel.items.push(item);
     },
     removeItem: function(item){
-    viewModel.items.remove(item);
+      viewModel.items.remove(item);
+    },
+    createItem: function(){
+      var item = viewModel.currentText();
+      viewModel.items.push(item);
+      viewModel.currentText('');
     }
   };
 
   this.viewModel = viewModel;
+}
+
+function TodoItem(value, remove) {
+  this.value = ko.observable(value);
+  this.remove = remove;
+  this.mode = ko.observable('view');
+  this.complete = ko.observable(false);
+}
+
+TodoItem.prototype.destroy = function(){
+  this.remove(this);
+};
+
+TodoItem.prototype.edit = function(){
+  this.mode('edit');
+}
+
+TodoItem.prototype.save = function(){
+  this.mode('view');
 }
--- script/viewModelTests.js
+++ script/viewModelTests.js
@@ -1,13 +1,9 @@
 (function(){
   var viewModel;
 
-  function tests(){
+  function viewModelTests(){
     function setUp(){
       viewModel = new App().viewModel;
-    }
-
-    function testShouldExist(){
-      assertNotUndefined(viewModel);
     }
 
     function testShouldAdd(){
@@ -22,7 +18,46 @@
       assertEqual(0,viewModel.items().length);
     }
 
+    function testShouldCreateItemFromCurrentText(){
+      viewModel.currentText('Oh Snap');
+      viewModel.createItem();
+      assertEqual(1,viewModel.items().length);
+    }
   }
 
-  this.viewModelTests = tests;
+  var todoItem;
+  function todoItemTests(){
+    function setUp(){
+      todoItem = new TodoItem('yo');
+    }
+
+    function testDestroyInvokesRemove(){
+      var hit = false;
+      todoItem.remove = function(){hit = true;};
+      todoItem.destroy();
+      assertTrue(hit);
+    }
+
+    function testDefaultsToViewMode(){
+      assertEqual('view', todoItem.mode());
+    }
+
+    function testEditChangesModeToEdit(){
+      todoItem.edit();
+      assertEqual('edit', todoItem.mode());
+    }
+
+    function testSaveChangesModeToView(){
+      todoItem.mode('other');
+      todoItem.save();
+      assertEqual('view', todoItem.mode());
+    }
+
+    function testCanComplete(){
+      todoItem.complete(true);
+    }
+  }
+
+
+  this.unitTests = [viewModelTests, todoItemTests];
 }());
